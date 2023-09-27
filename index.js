df84cb719cc7f27b5b75244b1c6af0880827143d
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2023-09-27 21:19:56 +0530

    All project added and implemeted

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ window.addEventListener( "mousemove" , (values)=>{
 
 
 
-
-
-
+// // // Menu position (Place) ----->
 let menuBtnClicked = false
 
 function showMenuBtn() {
@@ -39,6 +37,159 @@ function showMenuBtn() {
 
 
 
+// // // // Read mouse over on about section and change color of image background --->
+
+function getRandomColor(){
+
+    let colorArr = [
+        // "#30323D" ,
+        // "darkblue" ,
+        "#DB7F8E" ,
+        "#E8C547" ,
+        "#001514" ,
+        "#F15BB5" ,
+        "#03F7EB" ,
+        "#6B0504" ,
+        "#388697" ,
+        "#F40000" ,
+        "#FA8334" ,
+
+    ]
+    return colorArr[Math.floor( Math.random() * colorArr.length )]
+}
+
+
+function setRandomColorToRightOfAbout(){
+    document.getElementById("my_pic").style.backgroundColor = getRandomColor()
+}
+
+document.querySelector("#about").addEventListener("mousemove" , (dets)=>{
+
+    // console.log("In About Section" , getRandomColor())
+
+    setRandomColorToRightOfAbout()
+})
+
+
+// // // I want random color to right of about during the on load (On very first time)  --------->
+setRandomColorToRightOfAbout()
+
+
+
+
+
+
+// // // Hover and create div with image ----->
+
+
+
+const throttleFunction = (func, delay) => {
+
+    // Previously called time of the function
+    let prev = 0;
+    return (...args) => {
+        // Current called time of the function
+        let now = new Date().getTime();
+
+        // Logging the difference between previously
+        // called and current called timings
+        // console.log(now - prev, delay);
+
+        // If difference is greater than delay call
+        // the function again.
+        if (now - prev > delay) {
+            prev = now;
+
+            // "..." is the spread operator here
+            // returning the function with the
+            // array of arguments
+            return func(...args);
+        }
+    }
+}
+
+
+
+
+function randomImg() {
+    let images = [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCe96xcjYnpr4twMSox-dSbJraEvvknk2wjg&usqp=CAU",
+        "https://cdn.w600.comps.canstockphoto.com/colorful-flower-on-black-background-drawing_csp8445899.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3E7HfWciCppTVj2EGrBUt34FtRgsfNMfYzw&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDAVof4m0fMjXXItHlKeAchup0UEcpOZlTxQ&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeHfzNV8NN2-Qkwk1nQP-mLqHJ0hv6sh6vsQ&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFUQmeEdv_i7t0ojA0UbzTg9XhKIz4FQPtVA&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL4EZYAyZmo19iI-KwaW4X_Zj6ZyJ34ywKaA&usqp=CAU",
+        "https://res.cloudinary.com/dlvq8n2ca/image/upload/v1692032164/utemmzfh8jy0w4bufdp4.png",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtSyt2AuaENY8PrFMU_e2VKzSkDFj6w5wqdQ&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpip3aneCiD5G9DGvLl_9aCVkciA3rlt1RXw&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsik0vIMpdJlFgf3ElDxQJKYqlNNc4yICcV6O6FoMHevwhineR_aslFq0QUUq4gsvqxYc&usqp=CAU",
+        "https://img.freepik.com/premium-photo/realistic-image-eye-iris-cornea-retina-with-luminous-flash-light-blue-eye-3d-illustration_508524-254.jpg?w=2000",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu_fpPmbK-bebEeX036y7frmW06amtCkG1ew&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpcd08Atj6O4yBZDzCJp7H0msdHXc10gNr10KT9zyAthRC58FXlfArta_6fYDq0RIBcEo&usqp=CAU" ,
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9IFzxKtkXWiPakqTSyEIUsxf3qrTvksRPPw&usqp=CAU",
+
+    ];
+
+
+    return images[Math.floor(Math.random() * images.length)];
+
+}
+
+
+
+document.querySelector("#project_show").addEventListener("mousemove" ,  throttleFunction( (d)=>{
+
+
+    // console.log(d.clientX)
+    
+    let createdDiv = document.createElement("div")
+    createdDiv.classList.add("careted_div_by_js")
+    createdDiv.style.left = d.clientX-50+ "px"
+    createdDiv.style.top = d.clientY-70+ "px"
+    // console.log(createdDiv)
+
+
+    let img = document.createElement("img");
+    img.setAttribute( "src" , randomImg() );
+
+    createdDiv.appendChild(img)
+
+    // // console.log(div)
+
+    // img.style.transform = "translateY(0%)"
+    // img.style.transform = "rotate(50deg)"
+
+
+    // console.log(createdDiv)
+
+    gsap.to( img  , {
+        y : 0 ,
+        
+    })
+
+
+    document.querySelector("body").appendChild(createdDiv)
+
+    setTimeout(() => {
+        createdDiv.remove()
+    }, 1200)
+
+} , 270))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // let aboutSection = document.getElementById("about")
@@ -51,10 +202,11 @@ function showMenuBtn() {
 
 
 
+// // // Conect code ----------------->
+
+
 let emailRjex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 let nameRejex = /^[A-Za-z\ss]{1,35}$/
-
-
 
 
 // // // ***************************************************** Contact handler function **************************************
